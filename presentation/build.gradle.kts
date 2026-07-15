0d6436ab6abadd2b6f2597095235cc7320d92ca1
Abdelrahman Rashed <abdelrahman.rashed.ali@gmail.com>
2026-07-15 13:38:03 +0300

    fix issues

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.carenest.presentation"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
