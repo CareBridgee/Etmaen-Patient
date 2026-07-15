fdc13c7bb29560bee935832862c63d40ba7a99d3
Hendsayed25 <hendsayed853@gmail.com>
2026-07-15 15:46:38 +0300

    restore some colors

--- designsystem/src/main/java/com/carenest/designsystem/theme/ColorScheme.kt
+++ designsystem/src/main/java/com/carenest/designsystem/theme/ColorScheme.kt
@@ -27,7 +27,25 @@ data class ColorScheme(
     val onAmber: Color,
     val disable: Color,
     val onDisable: Color,
-    val tint: Color
+    val tint: Color,
+    val successContainer: Color,
+    val onSuccessContainer: Color,
+    val warningContainer: Color,
+    val onWarningContainer: Color,
+    val primaryContainer: Color,
+    val onPrimaryContainer: Color,
+    val infoContainer: Color,
+    val onInfoContainer: Color,
+    val bankContainer: Color,
+    val onBankContainer: Color,
+    val vodafoneContainer: Color,
+    val onVodafoneContainer: Color,
+    val processingContainer: Color,
+    val onProcessingContainer: Color,
+    val errorContainer: Color,
+    val onErrorContainer: Color,
+    val track: Color,
+    val divider: Color
 )
 
 val lightColors = ColorScheme(
@@ -54,7 +72,25 @@ val lightColors = ColorScheme(
     onAmber = Color(0xFFFFFFFF),
     disable = Color(0xFFEDEEEF),           // surface-container
     onDisable = Color(0xFFBDC9CA),         // outline-variant
-    tint = Color(0xFF006970),              // surface-tint
+    tint = Color(0xFF006970),
+    successContainer = Color(0xFFE8F5E9),
+    onSuccessContainer = Color(0xFF2E7D32),
+    warningContainer = Color(0xFFFFF3E0),
+    onWarningContainer = Color(0xFFEF6C00),
+    primaryContainer = Color(0xFFD3E8E6),
+    onPrimaryContainer = Color(0xFF004D40),
+    infoContainer = Color(0xFFF5F5F5),
+    onInfoContainer = Color(0xFF636369),
+    bankContainer = Color(0xFFE0F2F1),
+    onBankContainer = Color(0xFF004D40),
+    vodafoneContainer = Color(0xFFFFF1F0),
+    onVodafoneContainer = Color(0xFFD32F2F),
+    processingContainer = Color(0xFFE3F2FD),
+    onProcessingContainer = Color(0xFF1976D2),
+    errorContainer = Color(0xFFFFEBEE),
+    onErrorContainer = Color(0xFFD32F2F),
+    track = Color(0xFFEBEBEB),
+    divider = Color(0xFFF0F0F0)
 )
 
 val darkColors = ColorScheme(
@@ -82,6 +118,24 @@ val darkColors = ColorScheme(
     disable = Color(0xFF3E494A),
     onDisable = Color(0xFF6E797A),
     tint = Color(0xFF7CD4DD),
+    successContainer = Color(0xFF1B2E1E),
+    onSuccessContainer = Color(0xFF81C784),
+    warningContainer = Color(0xFF2E200D),
+    onWarningContainer = Color(0xFFFFD54F),
+    primaryContainer = Color(0xFF1B2E2E),
+    onPrimaryContainer = Color(0xFF80CBC4),
+    infoContainer = Color(0xFF2C2C2C),
+    onInfoContainer = Color(0xFFAAAAAA),
+    bankContainer = Color(0xFF002D2D),
+    onBankContainer = Color(0xFF80CBC4),
+    vodafoneContainer = Color(0xFF2D0A0A),
+    onVodafoneContainer = Color(0xFFE57373),
+    processingContainer = Color(0xFF0D1B2D),
+    onProcessingContainer = Color(0xFF64B5F6),
+    errorContainer = Color(0xFF2D0A0A),
+    onErrorContainer = Color(0xFFE57373),
+    track = Color(0xFF2C2C2C),
+    divider = Color(0xFF2C2C2C)
 )
 
 internal val localSPColorScheme = staticCompositionLocalOf { darkColors }
