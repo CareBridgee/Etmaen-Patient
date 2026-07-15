22088457af5e4250c0b0f97b84165a6b36ff3dab
wahid <wahidqandill@gmail.com>
2026-07-16 01:15:54 +0300

    Update Compose dependencies and configuration for presentation module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kover)
 }
 
@@ -16,19 +17,19 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        compose = true
+    }
 
 }
 
 dependencies {
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.material)
-    implementation(libs.bundles.compose)
-    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.androidx.core)
     implementation(project(":domain"))
+    implementation(project(":designsystem"))
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
-
-
 }
