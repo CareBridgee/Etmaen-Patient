b51837d2759d5486a9cab0047926cd7cf73d2553
Aalaa-Adel <aalaaadel02@gmail.com>
2026-07-16 03:41:01 +0300

    add data layer dependencies

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.androidx.room)
     alias(libs.plugins.kover)
 }
 
@@ -22,4 +26,56 @@ dependencies{
 
     implementation(project(":domain"))
 
+    implementation(libs.androidx.core.ktx)
+
+    // Ktor
+    implementation(libs.bundles.ktor)
+
+    // JSON Serialization
+    implementation(libs.kotlinx.serialization)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Date and time
+    implementation(libs.kotlinx.datetime)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+
+    // DataStore
+    implementation(libs.androidx.datastore.preferences)
+
+    // WorkManager
+    implementation(libs.androidx.work.runtime.ktx)
+
+    // Hilt with WorkManager
+    implementation(libs.androidx.hilt.work)
+    ksp(libs.androidx.hilt.compiler)
+
+    // Paging data source
+    implementation(libs.androidx.paging.runtime)
+
+    // Room + Paging integration
+    implementation(libs.androidx.room.paging)
+
+    // Device location
+    implementation(libs.play.services.location)
+
+    // Unit tests
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.ktor.client.mock)
+    testImplementation(libs.androidx.room.testing)
+
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
 }
