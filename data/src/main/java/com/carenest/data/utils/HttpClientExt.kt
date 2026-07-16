363d38a6bf69fd72f614f209de1b3945ecc679a7
wahid <wahidqandill@gmail.com>
2026-07-16 13:48:58 +0300

    Add getResults extension to HttpClient

---
+++ data/src/main/java/com/carenest/data/utils/HttpClientExt.kt
@@ -0,0 +1,26 @@
+package com.carenest.data.utils
+
+/**
+ * Author: Wahid Ali Wahid Hussien
+ * Created: 16/07/2026
+ */
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.request
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+
+suspend inline fun <reified T> HttpClient.getResults(
+    block: HttpRequestBuilder.() -> Unit
+): Result<T> = try {
+    val response = request(block)
+    if (response.status == HttpStatusCode.OK) {
+        Result.success(response.body())
+    } else {
+        Result.failure(Throwable("${response.status}: ${response.bodyAsText()}"))
+    }
+} catch (e: Exception) {
+    Result.failure(e)
+}
