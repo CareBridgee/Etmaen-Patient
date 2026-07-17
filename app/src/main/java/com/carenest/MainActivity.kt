0a95f85627685b70cab753fec59bc3ebcdc6ca16
wahid <wahidqandill@gmail.com>
2026-07-17 08:55:37 +0300

    Remove default theme and unused code

--- app/src/main/java/com/carenest/MainActivity.kt
+++ app/src/main/java/com/carenest/MainActivity.kt
@@ -18,25 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CareNestPatientTheme {
                 AppNav()
-            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CareNestPatientTheme {
-        Greeting("Android")
     }
 }
