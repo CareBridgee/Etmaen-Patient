e459fa5529f1d55da2622a8b6984366e6fe7892d
Aalaa-Adel <aalaaadel02@gmail.com>
2026-07-23 18:56:51 +0300

    Add TemporaryCompleteProfileTestConfig with hardcoded credentials for testing

---
+++ domain/src/main/java/com/carenest/domain/config/TemporaryCompleteProfileTestConfig.kt
@@ -0,0 +1,12 @@
+package com.carenest.domain.config
+
+/**
+ * TODO: Remove TemporaryCompleteProfileTestConfig before committing or pushing.
+ * Restore authenticated ProfileResponse.id and persisted access token from the OTP flow.
+ */
+object TemporaryCompleteProfileTestConfig {
+    const val ENABLED = true
+    const val PROFILE_ID = "c6eed55c-62e6-4b5f-ba71-195d78b928dc"
+    const val ACCESS_TOKEN =
+        "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1Yjk5NjM1MC0zODVlLTQ0MTgtOWZkMi02Y2YzNzdjNjEwZWUiLCJ0eXBlIjoiYWNjZXNzIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE3ODQ4MjAyMzIsImV4cCI6MTc4NDgyMTEzMn0.2ZlnsM9l3vMsErOGUWw2aaqNeBBhID23c6cz41eh898"
+}
