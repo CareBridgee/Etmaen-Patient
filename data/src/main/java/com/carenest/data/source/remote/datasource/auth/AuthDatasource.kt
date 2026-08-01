67da570fa413fb21d9fbeee756c6666f5fd02d4c
wahid <wahidqandill@gmail.com>
2026-08-01 22:59:36 +0300

    Implement token refresh functionality and update AuthApiService to use AuthHttpClient qualifier

--- data/src/main/java/com/carenest/data/source/remote/datasource/auth/AuthDatasource.kt
+++ data/src/main/java/com/carenest/data/source/remote/datasource/auth/AuthDatasource.kt
@@ -3,9 +3,11 @@ package com.carenest.data.source.remote.datasource.auth
 
 import com.carenest.data.source.remote.dto.AuthResponse
 import com.carenest.data.source.remote.dto.LoginResponse
+import com.carenest.data.source.remote.dto.TokenPairResponse
 
 interface AuthDatasource {
     suspend fun loginWithPhone(phoneNumber: String): Result<Unit>
     suspend fun requestDevOtp(phoneNumber: String): Result<LoginResponse>
     suspend fun verifyOtp(phoneNumber: String, otp: String): Result<AuthResponse>
+    suspend fun refreshToken(refreshToken: String): Result<TokenPairResponse>
 }
