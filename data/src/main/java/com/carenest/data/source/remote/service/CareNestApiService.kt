abc315d231e4f6df85b43acb3b6507ecdf8b0b54
Hendsayed25 <hendsayed853@gmail.com>
2026-08-06 14:28:29 +0300

    update get user requests  history by real data

--- data/src/main/java/com/carenest/data/source/remote/service/CareNestApiService.kt
+++ data/src/main/java/com/carenest/data/source/remote/service/CareNestApiService.kt
@@ -1,8 +1,10 @@
 package com.carenest.data.source.remote.service
 
 import com.carenest.data.source.remote.dto.ServiceDto
+import com.carenest.data.source.remote.dto.history.ServiceHistoryDto
 
 interface CareNestApiService {
      suspend fun getServices(): Result<List<ServiceDto>>
      suspend fun getServiceDetails(serviceId: String): Result<ServiceDto>
+     suspend fun getServiceHistory(): Result<List<ServiceHistoryDto>>
 }
