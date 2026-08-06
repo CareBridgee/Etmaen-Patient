8828d7352ba79cba95aa55b2ce4e0dbb8fe6cae4
Aalaa-Adel <aalaaadel02@gmail.com>
2026-08-07 01:18:57 +0300

    Implement profile image upload functionality and add top bar to WalletScreen

--- data/src/main/java/com/carenest/data/source/remote/service/UserApiService.kt
+++ data/src/main/java/com/carenest/data/source/remote/service/UserApiService.kt
@@ -5,5 +5,6 @@ import com.carenest.data.source.remote.dto.user.UserResponseDto
 
 interface UserApiService {
     suspend fun getCurrentUser(): Result<UserResponseDto>
+    suspend fun uploadProfileImage(fileName: String, contentType: String, bytes: ByteArray): Result<String>
     suspend fun updateCurrentUser(request: UpdateUserRequestDto): Result<UserResponseDto>
 }
