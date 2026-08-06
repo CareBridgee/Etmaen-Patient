44246d13e934077957aa29bd587d716fcbe52bb7
Aalaa-Adel <aalaaadel02@gmail.com>
2026-08-07 01:13:23 +0300

    Update ProfileMedicalConditionRequestDto with optional fields and add ProfileMedication domain model

--- domain/src/main/java/com/carenest/domain/model/profile/Medication.kt
+++ domain/src/main/java/com/carenest/domain/model/profile/Medication.kt
@@ -5,3 +5,10 @@ data class MedicationInput(
     val uiKey: Long,
     val name: String = ""
 )
+
+data class ProfileMedication(
+    val id: String,
+    val profileId: String,
+    val medicationId: String?,
+    val name: String
+)
